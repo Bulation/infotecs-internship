c597b9b1b0e4976affb2d18bb59d65c41d970659
Bulat Garipov <insanbulat@gmail.com>
2023-02-16 14:26:31 +0300

    refactor: remove table header item

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://63ea06f9e0ac9368d6487f93.mockapi.io/json';
 export const NOT_FOUND_CODE_STATUS = 404;
-export const TABLE_HEADER = ['firstName', 'lastName', 'phone', 'about', 'eyeColor'];
+export const TABLE_HEADER = ['firstName', 'lastName', 'about', 'eyeColor'];
 export const COUNT_PER_PAGE = 10;
 export const COLUMNS_COUNT = 4;
