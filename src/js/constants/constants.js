e2bf9c56fe5b9cdc6a8988e3d1122d760bf1dc57
Bulation <insanbulat@gmail.com>
2023-02-20 19:53:10 +0300

    feat: add constant for colors

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -3,3 +3,9 @@ export const NOT_FOUND_CODE_STATUS = 404;
 export const TABLE_HEADER = ['firstName', 'lastName', 'about', 'eyeColor'];
 export const COUNT_PER_PAGE = 10;
 export const COLUMNS_COUNT = 4;
+export const COLORS_NAME = {
+  'blue': '#0000ff',
+  'brown': '#a52a2a',
+  'red': '#ff0000',
+  'green': '#008000',
+};
