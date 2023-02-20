b2bb9a2b4057e00ead0b9b10fca8a91008f7040e
Bulation <insanbulat@gmail.com>
2023-02-20 18:40:43 +0300

    feat: add handler for sending data, router and local storage

--- src/js/controller/controller.js
+++ src/js/controller/controller.js
@@ -1,31 +1,51 @@
+import StorageModel from "../model/StorageModel.js";
+import Router from "../router/router.js";
+
 export default class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+    this.storageModel = new StorageModel();
+    this.loadDataFromStorage();
     this.registerListeners();
     this.view.renderPage();
-    this.model.loadData();
+    this.model.loadData().then(() => {
+      this.model.onUpdate(this.model.sliceData());
+      this.router = new Router(this.model, () => {
+        this.changeDataView();
+        this.model.onUpdate(this.model.sliceData());
+      });
+    });
+  }
+
+  loadDataFromStorage() {
+    if (this.storageModel.hasKey('hiddenColumnIndexes')) {
+      this.view.hiddenColumnIndexes = this.storageModel.load('hiddenColumnIndexes');
+    }
+  }
+
+  changeDataView() {
+    this.view.destroyTable();
+    this.view.renderPreloader();
   }
 
   registerListeners() {
     this.view.paginationClickHandle = (term) => {
-      this.model.pageNumber += term;
-      this.view.destroyTable();
-      this.view.renderPreloader();
-      this.model.normalizeData();
+      location.hash = this.model.pageNumber + term;
     }
     this.view.onSort = (name) => {
       this.model.sortName = name;
       this.model.changeOrders(name);
-      this.model.pageNumber = 0;
-      this.view.destroyTable();
-      this.view.renderPreloader();
-      this.model.normalizeData();
+      location.hash = 1;
+      this.changeDataView();
+      this.model.sortData();
+      this.model.onUpdate(this.model.sliceData());
     }
-    this.view.sendData = (peopleDataItem, id) => {
-      this.model.postData(peopleDataItem, id);
-      this.view.destroyTable();
-      this.view.renderPreloader();
+    this.view.sendData = async (peopleDataItem, id) => {
+      this.changeDataView();
+      await this.model.postData(peopleDataItem, id);
+      await this.model.loadData();
+      this.model.onUpdate(this.model.sliceData());
     }
     this.model.onUpdate = (peopleData) => {
       this.view.destroyErrorMessage();
@@ -36,6 +56,9 @@ export default class Controller {
     this.model.onError = (msg) => {
       this.view.destroyPreloader();
       this.view.renderErrorMessage(msg);
+    }
+    window.onbeforeunload = () => {
+      this.storageModel.save('hiddenColumnIndexes', this.view.hiddenColumnIndexes);
     }
   }
 }
