dd77966bf6194ee3cb24b88d0bc729a88449b160
Bulation <insanbulat@gmail.com>
2023-02-23 21:28:20 +0300

    feat: add configs

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  entry: './src/ts/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle[hash].js',
+    assetModuleFilename: '[hash][ext][query]',
+    clean: true,
+  },
+  devtool: !isProduction ? 'source-map' : false,
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    host: 'localhost',
+    hot: true,
+    static: {
+      directory: path.join(__dirname, './src'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
