b8c6a2079d6721e58c6e7539410fd3c5d1fde2c1
Bulation <insanbulat@gmail.com>
2023-02-23 18:48:04 +0300

    feat: add aria attributes

--- src/js/view/PaginationView.js
+++ src/js/view/PaginationView.js
@@ -3,11 +3,17 @@ import Component from "../common/component.js";
 export default class PaginationView extends Component {
   constructor(parentNode, tagName, className = '', content = '', onPaginationClick) {
     super(parentNode, tagName, className, content);
-    this.prevButton = new Component(this.node, 'button', 'btn prev-btn', 'Prev')
-      .setListener('click', () => onPaginationClick(-1));
-    this.pageNumber = new Component(this.node, 'div', 'page-number', '1');
-    this.nextButton = new Component(this.node, 'button', 'btn next-btn', 'Next')
-      .setListener('click', () => onPaginationClick(1));
+    this.prevButton = new Component(this.node, 'button', 'btn pagination__prev', 'Prev')
+      .setListener('click', () => onPaginationClick(-1))
+      .setAttribute('aria-label', 'Move to previous page')
+      .setAttribute('title', 'Move to previous page');
+    this.pageNumber = new Component(this.node, 'div', 'pagination__page-number', '1')
+      .setAttribute('aria-label', 'Page number')
+      .setAttribute('title', 'Page number');
+    this.nextButton = new Component(this.node, 'button', 'btn pagination__next', 'Next')
+      .setListener('click', () => onPaginationClick(1))
+      .setAttribute('aria-label', 'Move to next page')
+      .setAttribute('title', 'Move to next page');
   }
 
   setPageNumber(number) {
