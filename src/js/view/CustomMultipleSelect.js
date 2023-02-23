66bd025de0d4c5f06c4328dfa9c46668f004bb39
Bulation <insanbulat@gmail.com>
2023-02-23 18:46:22 +0300

    feat: add attributes and logic for checkboxes

--- src/js/view/CustomMultipleSelect.js
+++ src/js/view/CustomMultipleSelect.js
@@ -1,10 +1,12 @@
 import Component from "../common/component.js";
 
 export default class CustomMultipleSelect extends Component {
-  constructor(parentNode, tagName, className = '', content = '', options, onSelect, hiddenIndexes) {
+  constructor(parentNode, tagName, className = '', content = '', options, onSelect, checkedIndexes) {
     super(parentNode, tagName, className, content);
+    this.setAttribute('role', 'select');
     this.isOptionsOpen = false;
-    this.placeholder = new Component(this.node, 'div', 'select-placeholder', 'Hide column').setListener('click', () => {
+    this.checkedIndexes = checkedIndexes.slice();
+    this.placeholderContainer = new Component(this.node, 'div', 'select-placeholder', '').setListener('click', () => {
       if (this.isOptionsOpen) {
         this.optionsList.destroy();
         this.isOptionsOpen = false;
@@ -14,18 +16,28 @@ export default class CustomMultipleSelect extends Component {
         options.forEach((content, i) => {
           const option = new Component(this.optionsList.node, 'li', 'select-options-list__option', content).setListener('click', () => {
             onSelect(i);
-            if (checkbox.hasClass('selected')) {
-              checkbox.removeClass('selected');
+            if (checkbox.hasClass('select-options-list__option-checkbox_selected')) {
+              this.checkedIndexes = this.checkedIndexes.filter((value) => value !== i);
+              checkbox.removeClass('select-options-list__option-checkbox_selected');
+              сheckbox.setAttribute('aria-checked', 'false');
+              option.setAttribute('aria-selected', 'false');
             } else {
-              checkbox.setClass('selected');
+              this.checkedIndexes.push(i);
+              checkbox.setClass('select-options-list__option-checkbox_selected');
+              сheckbox.setAttribute('aria-checked', 'true');
+              option.setAttribute('aria-selected', 'true');
             }
-          })
-          const checkbox = new Component(option.node, 'span', 'select-options-list__option-checkbox', '');
-          if (hiddenIndexes.find((value) => value === i)) {
-            checkbox.setClass('selected');
+          }).setAttribute('role', 'option')
+          const checkbox = new Component(option.node, 'span', 'select-options-list__option-checkbox', '').setAttribute('role', 'checkbox')
+          if (this.checkedIndexes.find((value) => value === i) !== undefined) {
+            checkbox.setClass('select-options-list__option-checkbox_selected');
           }
         });
       }
     });
+    this.placeholder = new Component(this.placeholderContainer.node, 'div', 'select-placeholder__text', 'Hide column')
+      .setAttribute('aria-label', 'Open select')
+      .setAttribute('title', 'Open select');
+    this.placeholderIcon = new Component(this.placeholderContainer.node, 'span', 'select-placeholder__icon', '');
   }
 }
