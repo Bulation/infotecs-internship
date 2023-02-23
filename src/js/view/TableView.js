5090a4ac7b652bf75b1f1095182ffd0b85097ca7
Bulation <insanbulat@gmail.com>
2023-02-23 18:49:16 +0300

    feat: add classes and attributes

--- src/js/view/TableView.js
+++ src/js/view/TableView.js
@@ -2,24 +2,28 @@ import Component from "../common/component.js";
 import { COUNT_PER_PAGE, TABLE_HEADER } from "../constants/constants.js";
 
 export default class TableView extends Component {
-  constructor(parentNode, tagName, className = '', content = '', hiddenColumnIndexes, onRowClick, onSort) {
+  constructor(parentNode, tagName, className = '', content = '', hiddenColumnIndexes, onRowClick, onSort, ordersType) {
     super(parentNode, tagName, className, content);
     this.columns = [...new Array(4).fill([]).map(() => [])];
     this.hiddenColumnIndexes = hiddenColumnIndexes;
     this.onRowClick = onRowClick;
     this.onSort = onSort;
+    this.ordersType = ordersType;
   }
 
   renderTableHead() {
     this.thead = new Component(this.node, 'thead', 'table__head', '');
     new Component(this.thead.node, 'th', 'table__head-item', '');
     TABLE_HEADER.forEach((name, i) => {
-      const tableHead = new Component(this.thead.node, 'th', 'table__head-item', name);
+      const tableHead = new Component(this.thead.node, 'th', 'table__head-item', name)
+        .setClass(this.ordersType[name] === 'asc' ? 'table__head-item_asc' : 'table__head-item_desc')
+        .setListener('click', () => {
+          this.onSort(name);
+          this.toggleSortClass(tableHead, i);
+        })
+        .setAttribute('aria-label', 'Sort column')
+        .setAttribute('title', 'Sort column');
       this.columns[i].push(tableHead);
-      tableHead.setListener('click', () => {
-        this.onSort(name);
-        this.toggleSortClass(tableHead, i);
-      });
     });
   }
 
@@ -28,10 +32,25 @@ export default class TableView extends Component {
     peopleData.forEach((peopleItem, index) => {
       const row = new Component(this.tbody.node, 'tr', 'table__row', '');
       new Component(row.node, 'td', 'table__col', String(index + 1 + (pageNumber - 1) * COUNT_PER_PAGE));
-      this.columns[0].push(new Component(row.node, 'td', 'table__col', `${peopleItem.name.firstName}`));
-      this.columns[1].push(new Component(row.node, 'td', 'table__col', `${peopleItem.name.lastName}`));
-      this.columns[2].push(new Component(row.node, 'td', 'table__col table__col-about', `${peopleItem.about}`));
-      const eye = new Component(row.node, 'td', 'table__col', '').setStyle('background-color', peopleItem.eyeColor)
+      this.columns[0].push(
+        new Component(row.node, 'td', 'table__col', `${peopleItem.name.firstName}`)
+        .setAttribute('aria-label', 'Open form for edit row')
+        .setAttribute('title', 'Open form for edit row')
+      );
+      this.columns[1].push(
+        new Component(row.node, 'td', 'table__col', `${peopleItem.name.lastName}`)
+        .setAttribute('aria-label', 'Open form for edit row')
+        .setAttribute('title', 'Open form for edit row')
+      );
+      this.columns[2].push(
+        new Component(row.node, 'td', 'table__col table__col-about', `${peopleItem.about}`)
+        .setAttribute('aria-label', 'Open form for edit row')
+        .setAttribute('title', 'Open form for edit row')
+      );
+      const eye = new Component(row.node, 'td', 'table__col', '')
+        .setStyle('background-color', peopleItem.eyeColor)
+        .setAttribute('aria-label', 'Open form for edit row')
+        .setAttribute('title', 'Open form for edit row');
       this.columns[3].push(eye);
       row.setListener('click', () => this.onRowClick(peopleItem, eye));
     });
@@ -39,14 +58,16 @@ export default class TableView extends Component {
 
 
   toggleSortClass(tableHead, headIndex) {
-    if (tableHead.hasClass('desc')) {
-      tableHead.removeClass('desc');
+    if (tableHead.hasClass('table__head-item_desc')) {
+      tableHead.removeClass('table__head-item_desc');
+      tableHead.setClass('table__head-item_asc');
     } else {
-      tableHead.setClass('desc');
+      tableHead.removeClass('table__head-item_asc');
+      tableHead.setClass('table__head-item_desc');
     }
     this.columns.forEach((column, columnIndex) => {
       if (headIndex !== columnIndex) {
-        column[0].removeClass('desc');
+        column[0].removeClass('table__head-item_desc');
       }
     })
   }
