973a94feee986c80815866f55f1fe6b80c3b08a6
Bulation <insanbulat@gmail.com>
2023-02-24 23:55:53 +0300

    feat: import styles

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,6 @@
 import Controller from './controller/controller';
 import DataModel from './model/dataModel';
 import AppView from './view/AppView';
+import "../css/style.css";
 
 new Controller(new DataModel(), new AppView(document.body)); // инициализируем контроллер, передаем в него модель и вьюху, в которую передаем body
