0b5c34e361816827fe920df6cc4e13c066ce1214
Bulation <insanbulat@gmail.com>
2023-02-24 23:45:20 +0300

    feat: rewrite tests to typescript

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,9 +1,9 @@
 import { enableFetchMocks } from 'jest-fetch-mock'
 enableFetchMocks()
-import { COUNT_PER_PAGE } from "../js/constants/constants";
-import DataModel from "../js/model/dataModel.js";
+import fetch from 'jest-fetch-mock';
+import { COUNT_PER_PAGE } from "../ts/constants/constants";
+import DataModel from "../ts/model/dataModel";
 import mockData from "./mockData.json";
-import rgbToHex from '../js/helperFunctions/rgbToHex';
 
 test('length of array is 50', async () => {
   fetch.mockResponseOnce(JSON.stringify(mockData))
@@ -17,8 +17,4 @@ test('length of sliced array is 10', async () => {
   const model = new DataModel();
   await model.loadData();
   expect(model.sliceData().length).toBe(COUNT_PER_PAGE);
-});
-
-test('rgb(124, 100, 43) is #7c642b', async () => {
-  expect(rgbToHex('rgb(124, 100, 43)')).toBe('#7c642b');
 });
